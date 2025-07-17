7da8d339e4da0c04000ecf1beaf51577011ec420
Harshad Panchani <harshad.panchani@tssconsultancy.com>
2025-07-17 15:36:10 +0530

    Streams Added

--- Streams_app/src/com/tss/test/ComparatorExample.java
+++ Streams_app/src/com/tss/test/ComparatorExample.java
@@ -6,38 +6,23 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
 import com.tss.model.Employee;
 
 public class ComparatorExample {
 	public static void main(String[] args) {
-        List<Employee> employees = Arrays.asList(
-            new Employee("Liam", "Finance", 75000),
-            new Employee("Olivia", "Engineering", 80000),
-            new Employee("Ethan", "Engineering", 75000),
-            new Employee("Sophia", "Marketing", 75000),
-            new Employee("Mason", "Finance", 85000)
-        );
+		List<Employee> employees = Arrays.asList(new Employee("Harshad", "Finance", 75000),
+				new Employee("Mahek", "Engineering", 80000), new Employee("Rishit", "Engineering", 75000),
+				new Employee("Ashish", "Marketing", 75000), new Employee("Deep", "Finance", 85000));
 
-        employees.sort(
-            Comparator.comparing(Employee::getSalary).reversed()
-                      .thenComparing(Employee::getName)
-        );
+		employees.sort(Comparator.comparing(Employee::getSalary).reversed().thenComparing(Employee::getName));
 
-        employees.forEach(System.out::println);
-        
-        Map<String, Employee> highestPaidByDept = employees.stream()
-                .collect(Collectors.groupingBy(
-                    Employee::getDepartment,
-                    Collectors.collectingAndThen(
-                        Collectors.maxBy(Comparator.comparingDouble(Employee::getSalary)),
-                        Optional::get
-                    )
-                ));
+		employees.forEach(System.out::println);
 
-            highestPaidByDept.forEach((dept, emp) -> 
-                System.out.println(dept + " => " + emp)
-            );
-    }
-	
+		Map<String, Employee> highestPaidByDept = employees.stream()
+				.collect(Collectors.groupingBy(Employee::getDepartment, Collectors.collectingAndThen(
+						Collectors.maxBy(Comparator.comparingDouble(Employee::getSalary)), Optional::get)));
+
+		highestPaidByDept.forEach((dept, emp) -> System.out.println(dept + " => " + emp));
+	}
+
 }

f414fd7607c2fc26ea29962e40107a075a8f9565
Harshad Panchani <harshad.panchani@tssconsultancy.com>
2025-07-28 15:47:43 +0530

    Database

--- studentdb_app/src/com/tss/service/StudentService.java
+++ studentdb_app/src/com/tss/service/StudentService.java
@@ -19,7 +19,20 @@ public class StudentService {
 	}
 
 	public void addStudent(Student student) {
+		
+		if(student.getAge()<18)
+		{
+			System.out.println("Age Must Be Greater Than 18 !!");
+			return;
+		}
+		if(student.getPercentage()<0)
+		{
+			System.out.println("Percentage Must Be Positive !!");
+			return;
+		}
 		studentDao.addNewStudent(student);
+
+		
 	}
 
 }
